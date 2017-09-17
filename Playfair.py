572be3b0f1a9df938bea400d4d4ae06ff74ad346
ThejasN <thejasn.416@gmail.com>
2017-09-17 19:11:33 +0530

    generation of key matrix done

--- Playfair.py
+++ Playfair.py
@@ -2,7 +2,31 @@ import sys
 import numpy as np 
 def_char = 'x'
 def_char2 = 'z'
+alphabets="abcdefghijklmnopqrstuvwxyz"
 param = sys.argv[1]
+
+def process_input(input):
+    processed=""
+    for i in range(0,len(input)-1):
+        if input[i] == input[i+1]:
+            processed+=input[i]
+            if input[i] == def_char:
+                processed+=def_char2
+            else:
+                processed+=def_char
+        else:
+            processed+=input[i]
+    if len(processed) % 2 ==0:
+        if input[len(input)-1]==def_char:
+            processed+=input[len(input)-1]+def_char2
+        else:   processed+=input[len(input)-1]+def_char; 
+    else: processed+=input[len(input)-1]
+     
+    processed = list(processed)
+    [processed.append(i) for i in alphabets if i not in processed] 
+    keymap = [[ processed[i+j*5] for i in range(0,5)] for j in range(0,5)]
+    return keymap
+
 if param == "enc":
     print(" Enter Key:",end="")
     key = str(input())
@@ -13,17 +37,5 @@ if param == "enc":
     key = unique
     print(" Plain Text:",end="")
     input = str(input())
-    for i in range(0,len(input)-1):
-        if input[i] == input[i+1]:
-            processed+=input[i]
-            if input[i] == def_char:
-                processed+=def_char2
-            else:
-                processed+=def_char
-        else:
-            processed+=input[i]
-    processed+=input[len(input)-1]
-
-    print(processed)
-
-
+    key_matrix = process_input(input)
+    
