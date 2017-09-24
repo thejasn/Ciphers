609bef01da77a08237dcfab22756e3e8c067bdb5
ThejasN <thejasn.416@gmail.com>
2017-09-24 11:54:06 +0530

    added Encryption

--- Playfair.py
+++ Playfair.py
@@ -2,10 +2,10 @@ import sys
 import numpy as np 
 def_char = 'x'
 def_char2 = 'z'
-alphabets="abcdefghijklmnopqrstuvwxyz"
+alphabets="abcdefghjklmnopqrstuvwxyz"
 param = sys.argv[1]
 
-def process_input(input):
+def process_key(input):
     processed=""
     for i in range(0,len(input)-1):
         if input[i] == input[i+1]:
@@ -23,10 +23,28 @@ def process_input(input):
     else: processed+=input[len(input)-1]
      
     processed = list(processed)
+    processed = [ i for i in processed if i != 'j']
     [processed.append(i) for i in alphabets if i not in processed] 
     keymap = [[ processed[i+j*5] for i in range(0,5)] for j in range(0,5)]
     return keymap
 
+def encrypt(message,keymap):
+    first=""
+    second=""
+    ciphered=""
+    for letter in range(1,len(message),2):
+        for i in range(0,5):
+            if message[letter-1] in key_matrix[i]:
+                first = key_matrix[i].index(message[letter-1])
+                ciphered += key_matrix[(i+1)%5][first]
+                break
+        for i in range(0,5):
+            if message[letter] in key_matrix[i]:
+                second = key_matrix[i].index(message[letter])
+                ciphered += key_matrix[(i+1)%5][second]
+                break
+
+    print(ciphered)
 if param == "enc":
     print(" Enter Key:",end="")
     key = str(input())
@@ -37,5 +55,6 @@ if param == "enc":
     key = unique
     print(" Plain Text:",end="")
     input = str(input())
-    key_matrix = process_input(input)
-    
+    key_matrix = process_key(key)
+    cipher_text = encrypt(input,key_matrix)
+    print(key_matrix)
